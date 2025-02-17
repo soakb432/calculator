d29129321014719db7c5d4078cc27f852098715b
soakb432 <107282874+soakb432@users.noreply.github.com>
2025-02-17 14:49:34 -0400

    Restore file

---
+++ script.js
@@ -0,0 +1,36 @@
+// -- OPERATION VARIABLES -- //
+
+let firstOperand;
+let operator;
+let secondOperand;
+
+// -- MATH OPERATIONS -- //
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, firstNum, secondNum) {
+    switch (operator) {
+        case "+":
+            return add(firstNum, secondNum);
+        case "-":
+            return subtract(firstNum, secondNum);
+        case "*":
+            return multiply(firstNum, secondNum);
+        case "/":
+            return divide(firstNum, secondNum);
+    }
+}
